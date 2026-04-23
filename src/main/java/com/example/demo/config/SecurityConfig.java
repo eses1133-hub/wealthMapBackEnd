610988a9878d602161320f1cda79076f35ddf6c2
roooooong <mandy0220405@gmail.com>
2026-04-23 16:42:58 +0800

    Update SecurityConfig.java
    調整位置 requestMatchers("/api/auth/send-mail").permitAll()

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -95,6 +95,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/sse/**").permitAll()
                 .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
                 .requestMatchers("/api/users/details/**").permitAll()
+                .requestMatchers("/api/auth/send-mail").permitAll()      // 1. 發信不用登入
                 // 3. 園區服務台 (Error)：放行
                 .requestMatchers("/error").permitAll()
                 // 4. 管理員辦公室：只有「園區經理」(ADMIN) 才能進
@@ -108,9 +109,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 // 6. 剩下的神祕區域，通通要檢查身分
                 
-                .requestMatchers("/api/auth/send-mail").permitAll()      // 1. 發信不用登入
-                .requestMatchers("/api/auth/login").permitAll()          // 2. 登入不用登入
-                .requestMatchers("/api/auth/register").permitAll()       // 3. 註冊不用登入
+                
                 .requestMatchers("/api/auth/change-password").authenticated() // 4. 修改密碼「必須」登入
                 .requestMatchers("/profile").authenticated()
                 .requestMatchers("/by-email").permitAll()
